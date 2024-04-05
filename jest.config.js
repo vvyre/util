1e4c6872ea2714e214da5ff13377c5f7ea610d35
brewcoldblue <seung.yoon.yu@gmail.com>
2024-04-05 11:37:00 +0900

    chore: install deps and scaffolding project

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  testRegex: '/__test__/.*\\.test\\.(ts|tsx)?$',
+  testPathIgnorePatterns: ['/node_modules/', '/e2e/'],
+  modulePathIgnorePatterns: ['<rootDir>/examples/'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '^swr/useform$': '<rootDir>/use-form/src/index.ts',
+    '^swr/_internal$': '<rootDir>/_internal/src/index.ts'
+  },
+  transform: {
+    '^.+\\.(t|j)sx?$': ['@swc/jest']
+  },
+  coveragePathIgnorePatterns: ['/node_modules/', '/dist/', '/test/'],
+  coverageReporters: ['text', 'html'],
+  reporters: [['github-actions', { silent: false }], 'summary']
+}
