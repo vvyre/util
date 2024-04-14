2fd21b53c0d8fa1611e0951f604d1d91f8704341
brewcold <seung.yoon.yu@gmail.com>
2024-04-14 14:37:31 +0900

    feat: add objectKeys and objectEntries with type assertion

---
+++ ts/src/object/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Alternative of Object.keys() with type assertion
+ * @example objectKeys({ a : 1 }) // ['a']
+ */
+export const objectKeys = <T extends Object>(obj: T) => Object.keys(obj) as Array<keyof T>
+
+/**
+ * Alternative of Object.Entries() with type assertion
+ * @example objectEntries({ a : 1 }) // [['a', 1]]
+ */
+export const objectEntries = <T extends Object>(obj: T) => Object.entries(obj) as Entries<T>
