884191559b04648ecc0e415cb661f5d2a296839a
brewcold <seung.yoon.yu@gmail.com>
2024-04-14 12:38:18 +0900

    chore: add path alias

--- jest.config.js
+++ jest.config.js
@@ -2,11 +2,9 @@ module.exports = {
   testEnvironment: 'jsdom',
   testRegex: '/__test__/.*\\.test\\.(ts|tsx)?$',
   testPathIgnorePatterns: ['/node_modules/', '/e2e/'],
-  modulePathIgnorePatterns: ['<rootDir>/examples/'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleNameMapper: {
-    '^swr/useform$': '<rootDir>/use-form/src/index.ts',
-    '^swr/_internal$': '<rootDir>/_internal/src/index.ts'
+    '^@/(.*)$': '<rootDir>/$1'
   },
   transform: {
     '^.+\\.(t|j)sx?$': ['@swc/jest']
