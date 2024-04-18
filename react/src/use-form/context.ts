542251710e4d9c209c81f909ae4e940f8dd1d694
brewcold <seung.yoon.yu@gmail.com>
2024-04-18 15:47:26 +0900

    feat: flexibility in return types and provision of initial values in context

--- react/src/use-form/context.ts
+++ react/src/use-form/context.ts
@@ -1,17 +1,24 @@
 import { Context, createContext, useContext } from 'react'
 import { UseForm } from './types'
+import { useForm } from '.'
 
 /**
  * createContext API that includes type definitions of useForm.
  */
 export function createUseFormContext<T extends Object>() {
-  const FormContext = createContext<Partial<UseForm<T> | null>>(null)
+  //Since the results of the hook need to be stored after it is called,
+  //it's not possible to store a default value at this point.
+  const FormContext = createContext<UseForm<T> | Partial<UseForm<T>> | null>(null)
   return FormContext
 }
 
 /**
  * useContext API that includes type definitions of useForm.
  */
-export function useFormContext<T extends Object>(arg: Context<Partial<UseForm<T> | null>>) {
-  return useContext<Partial<UseForm<T> | null>>(arg)
+export function useFormContext<T extends Object>(
+  arg: Context<UseForm<T> | Partial<UseForm<T>> | null>
+) {
+  const ctx = useContext<UseForm<T> | Partial<UseForm<T>> | null>(arg)
+  if (!ctx) return useForm<T>({ initialValues: {} as T, onSubmit: () => {} })
+  return ctx as UseForm<T>
 }
