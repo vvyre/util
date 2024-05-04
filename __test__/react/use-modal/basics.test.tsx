728b559f83860a3575c2dab57546dfcb6c76c925
brewcold <seung.yoon.yu@gmail.com>
2024-05-04 17:02:28 +0900

    test: add test ites for useModal

---
+++ __test__/react/use-modal/basics.test.tsx
@@ -0,0 +1,7 @@
+describe('useModal', () => {
+  it('should mount component when the open() function is called', () => {})
+
+  it('should unmount component when the close() function is called', () => {})
+
+  it('should unmount existing modal component when the open() function is called again', () => {})
+})
